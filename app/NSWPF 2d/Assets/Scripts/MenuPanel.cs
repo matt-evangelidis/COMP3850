9ba13d1d7fd4edbed4a890553757c85e38358c11
Quang Huy Ha <chauhacon@gmail.com>
2020-04-05 00:11:36 +1100

    making some change to main menu graphic

---
+++ app/NSWPF 2d/Assets/Scripts/MenuPanel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuPanel : MonoBehaviour
+{
+    public GameObject searching;
+    public GameObject account;
+
+    public void toAccount() {
+        SceneManager.LoadScene("Account");
+    }
+
+    public void toSearching() {
+        SceneManager.LoadScene("Module Searching");
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
