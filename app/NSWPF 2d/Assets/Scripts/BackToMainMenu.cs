cda01d894029b7d3afaf62a77c1634f9c9a91279
mqlnx <lin.xie1@students.mq.edu.au>
2020-04-13 16:04:17 +1000

    Button and script for going back to main menu

---
+++ app/NSWPF 2d/Assets/Scripts/BackToMainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class BackToMainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToMain()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
