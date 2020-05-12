9a7b8b0b2dc42513d87aad1ef9adf96314596178
Matthew Evangelidis <54091051+Notintheslightest@users.noreply.github.com>
2020-05-12 22:58:42 +1000

    Initial Quiz2
    Visual elements and scripts done for Quiz2, no back-end integration so far.

---
+++ app/NSWPF 2d/Assets/Scripts/Quiz2/Click.cs
@@ -0,0 +1,27 @@
+﻿using Shapes2D;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using UnityEngine.UIElements;
+
+public class Click : MonoBehaviour
+{
+    public GameObject areaClick;
+    public GameObject box;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Instantiate(areaClick, worldPosition, Quaternion.identity);
+        }
+    }
+}
---
+++ app/NSWPF 2d/Assets/Scripts/Quiz2/ClickArea.cs
@@ -0,0 +1,33 @@
+﻿using Shapes2D;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickArea : MonoBehaviour
+{
+    //script attached to pref
+    private GameObject self;
+    private float alpha = 1f;
+    SpriteRenderer temp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        self = gameObject;
+        temp = self.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //fade alpha to zero
+        temp.color = new Color(1,1,1, alpha);
+        alpha = alpha - 0.3f * Time.deltaTime;
+        
+        //destroy instance
+        if (alpha <= 0)
+        {
+            Destroy(self);
+        }
+    }
+}
