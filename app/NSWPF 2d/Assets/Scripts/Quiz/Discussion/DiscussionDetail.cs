95f25ba3f6d778da12c0b1afaf279ffaea6a80d4
Quang Huy Ha <chauhacon@gmail.com>
2020-05-18 01:58:10 +1000

    new design discussion forum

--- app/NSWPF 2d/Assets/Scripts/Quiz/Discussion/DiscussionDetail.cs
+++ app/NSWPF 2d/Assets/Scripts/Quiz/Discussion/DiscussionDetail.cs
@@ -10,23 +10,26 @@ using TMPro;
 
 public class DiscussionDetail : MonoBehaviour
 {
-    public GameObject detail;
-    public GameObject Reply;
-    public GameObject dividerContent;
-    public GameObject dividerReply;
+    public GameObject thread_heading;
+    public GameObject thread_nameRole;
+    public GameObject thread_content;
+
+    public GameObject entry;
     public GameObject warning;
-    public GameObject content;
+    public GameObject reply;
 
     public Discussion discussion;
 
     public static Thread post = null;
     private List<Reply> replies = null;
 
-    public void back() {
+    public void back()
+    {
         SceneManager.LoadScene("Searching Discussion");
     }
 
-    public void createReply() {
+    public void createReply()
+    {
         SceneManager.LoadScene("Discussion Reply");
     }
 
@@ -36,35 +39,42 @@ public class DiscussionDetail : MonoBehaviour
     {
         discussion = Discussion.getDiscussion();
 
-        post = discussion.getThread(SearchingDiscussion.postDetail.name);
-        if (post == null) {
+        if (SearchingDiscussion.postDetail == null)
+        {
             warning.GetComponent<Text>().text = "no post found";
             return;
         }
-        
-        detail.GetComponent<Text>().text = post.username + " (" + post.role + ") " + ": " + post.content;
 
-
-        replies = post.replies;
-
-        foreach (Reply reply in replies)
+        post = discussion.getThread(SearchingDiscussion.postDetail.name);
+        if (post == null)
         {
-            GameObject go = (GameObject)Instantiate(Reply);
-            go.transform.SetParent(content.transform);
-            go.GetComponent<Text>().text = reply.username + " (" +reply.role+ ") : " + reply.content;
-
-            GameObject divider = (GameObject)Instantiate(dividerReply);
-            divider.transform.SetParent(content.transform);
+            warning.GetComponent<Text>().text = "no post found";
+            return;
         }
 
+        // Display thread
+        thread_heading.GetComponent<Text>().text = post.heading;
+        thread_nameRole.GetComponent<Text>().text = post.username + " - " + post.role;
+        thread_content.GetComponent<Text>().text = post.content;
 
-        Reply.SetActive(false);
-        dividerReply.SetActive(false);
+        replies = post.replies;
+        
+        foreach (Reply re in replies)
+        {
+            GameObject go = (GameObject)Instantiate(entry);
+            go.transform.SetParent(reply.transform);
+
+            go.transform.Find("Header").Find("Heading").GetComponentInChildren<Text>().text = "Re: " + post.heading;
+            go.transform.Find("Header").Find("name-role").GetComponentInChildren<Text>().text = re.username + " - "+ re.role;
+            go.transform.Find("Content").GetComponentInChildren<Text>().text = re.content;
+        }
+
+        entry.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
